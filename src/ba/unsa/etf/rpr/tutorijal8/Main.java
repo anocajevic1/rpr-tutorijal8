5517b76dda0ad072c58d92d4c88b7073bd832067
anocajevic1 <anocajevic1@etf.unsa.ba>
2018-12-18 07:02:08 +0100

    Pocetni commit

--- src/ba/unsa/etf/rpr/tutorijal8/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal8/Main.java
@@ -1,8 +1,26 @@
 package ba.unsa.etf.rpr.tutorijal8;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Pretraga datoteka");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+    }
+
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
     }
 }
